c4ae5a8a264b8a2329028800aba71ec7c3a1a06b
Evan Sibout <evan.sibout@supinfo.com>
2025-11-24 15:31:02 +0100

    add : Tous les exercices jour 1

---
+++ script.js
@@ -0,0 +1,77 @@
+// // Maintenant que vous savez exécuter des scripts JS, nous allons reprendre d’anciens algorithmes et les adapter pour qu'ils soient compatibles avec JavaScript.
+
+// // 1. Calculer un prix
+// // Implémentez un programme qui :
+
+// // Demande à l'utilisateur de saisir un prix unitaire HT.
+// // Demande un taux de TVA.
+// // Demande un nombre d'articles.
+// // Calcule et affiche le montant TTC de l'achat.
+
+// let i = prompt('entrer un prix unitaire HT : ')
+// let j = prompt('entrer un taux de TVA : ')
+// let k = prompt('entrer un nombre darticle : ')
+
+// l = (i * j) * k;
+// console.log(l);
+
+// --------------------------------
+// // 2. Échange de deux variables
+// // Implémentez trois programmes qui réalisent l’échange des valeurs de deux variables saisies par l’utilisateur :
+
+// // En utilisant une affectation multiple.
+
+// // Sans utiliser d’affectation multiple.
+
+// // En utilisant les opérateurs + et -.
+
+// // (Cela sous-entend que les variables sont toutes deux numériques.)
+
+// let i = prompt('entrez une valeur : ')
+// let j = prompt('entrez une autre valeur : ')
+
+// res = i; 
+// i = j
+// j = res 
+// console.log(i,j)
+
+// ---------------------------------
+
+// let i = prompt('entrez une valeur : ')
+// let j = prompt('entrez une valeur : ')
+
+// res = i, i = j, j = res;
+// console.log(i,j)
+
+// ---------------------------------
+
+// let i = Number(prompt('entrez une valeur : '))
+// let j = Number(prompt('entrez une valeur : '))
+
+// i = i + j
+// j = i - j
+// i = i - j
+// console.log(i,j)
+
+// ----------------------------------
+// // 3. Échange de trois variables
+// // Implémentez un programme réalisant une permutation circulaire de trois variables x, y, z dont les valeurs sont saisies par l’utilisateur.
+
+// // Définition :
+// // À la fin du programme :
+
+// // x contient la valeur initiale de y.
+// // y contient la valeur initiale de z.
+// // z contient la valeur initiale de x
+
+// let i = prompt('entrer un prix unitaire HT : ')
+// let j = prompt('entrer un taux de TVA : ')
+// let k = prompt('entrer un nombre darticle : ')
+
+
+// i == j;
+// j == k;
+// k == i;
+
+// l = (i * j) * k;
+// console.log(l);
